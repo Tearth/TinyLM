a15fbe0a077c44e30622fa266713e5f79fd188b6
Tearth <tearthdev@gmail.com>
2026-04-29 15:03:16 +0200

    Add chunk stride

--- src/dataset.py
+++ src/dataset.py
@@ -5,9 +5,10 @@ from torch import Tensor
 from torch.utils.data import Dataset
 
 class ModelDataset(Dataset):
-    def __init__(self, token_dictionary: TokenDictionary, chunk_size: int) -> None:
+    def __init__(self, token_dictionary: TokenDictionary, chunk_size: int, stride: int) -> None:
         self.token_dictionary = token_dictionary
         self.chunk_size = chunk_size
+        self.stride = stride
         self.data = torch.empty(0)
         self.size = 0
 
@@ -28,11 +29,11 @@ class ModelDataset(Dataset):
         self.data = torch.tensor(buffer)
     
     def __getitem__(self, idx) -> tuple[Tensor, Tensor]:
-        position = idx * self.chunk_size
+        position = int(idx * self.stride)
         features = self.data[position:position+self.chunk_size]
         labels = self.data[position + 1:position+self.chunk_size + 1]
 
         return (features, labels)
 
     def __len__(self):
-        return int(len(self.data) / self.chunk_size) - 1
+        return int(len(self.data) - self.chunk_size) // self.stride
