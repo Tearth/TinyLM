d114b5c415aa357dd5b10f657b96afa55ad54185
Tearth <tearthdev@gmail.com>
2026-04-29 14:33:49 +0200

    Optimize dataset and switch to character per token

--- src/dataset.py
+++ src/dataset.py
@@ -8,34 +8,31 @@ class ModelDataset(Dataset):
     def __init__(self, token_dictionary: TokenDictionary, chunk_size: int) -> None:
         self.token_dictionary = token_dictionary
         self.chunk_size = chunk_size
-        self.chunks = []
+        self.data = torch.empty(0)
         self.size = 0
 
     def load(self, path: str):
-        pad_id = self.token_dictionary.encode_pad()
+        buffer = []
 
         with open(path, encoding="utf-8") as file:
             for line in file:
-                _, token_ids = self.token_dictionary.encode(line, False)
+                line = line.strip()
                 
-                start_position = 0
-                self.size += len(line)
+                if len(line) == 0 or line.isspace():
+                    continue
+                
+                for char in line:
+                    buffer.append(self.token_dictionary.encode(char, False))
+                buffer.append(self.token_dictionary.encode("\n", False))
 
-                while start_position + self.chunk_size <= len(token_ids):
-                    self.chunks.append(token_ids[start_position:start_position + self.chunk_size])
-                    start_position += self.chunk_size
-                
-                if start_position < len(token_ids):
-                    chunk = token_ids[start_position:]
-                    chunk.extend([pad_id] * (self.chunk_size - (len(token_ids) - start_position)))
-                    self.chunks.append(chunk)
+        self.data = torch.tensor(buffer)
     
     def __getitem__(self, idx) -> tuple[Tensor, Tensor]:
-        chunk = self.chunks[idx]
-        features = chunk[:]
-        labels = chunk[1:] + [self.token_dictionary.encode_pad()]
+        position = idx * self.chunk_size
+        features = self.data[position:position+self.chunk_size]
+        labels = self.data[position + 1:position+self.chunk_size + 1]
 
-        return torch.tensor(features), torch.tensor(labels)
+        return (features, labels)
 
     def __len__(self):
-        return len(self.chunks)
+        return int(len(self.data) / self.chunk_size) - 1
--- src/tokens.py
+++ src/tokens.py
@@ -1,38 +1,31 @@
-
 from bidict import bidict
 
-UNKNOWN_TOKEN = "<unknown>"
-PAD_TOKEN = "<pad>"
+UNKNOWN_TOKEN = "⊗"
+PAD_TOKEN = "•"
 
 class TokenDictionary:
     def __init__(self) -> None:
         self.map = bidict({
             UNKNOWN_TOKEN: 0,
-            PAD_TOKEN: 1,
+            PAD_TOKEN: 1
         })
-        self.next_id = 2
+        self.next_id = len(self.map)
 
-    def encode(self, content: str, read_only: bool) -> tuple[list[str], list[int]]:
-        tokens = content.split()
-        token_ids = []
+    def encode(self, token: str, read_only: bool) -> int:
+        if read_only:
+            id = self.map.get(token, self.map[UNKNOWN_TOKEN])
+        else:
+            id = self.map.get(token)
 
-        for token in tokens:
-            if read_only:
-                token_ids.append(self.map.get(token, self.map[UNKNOWN_TOKEN]))
-            else:
-                id = self.map.get(token)
-                
-                if id is None:
-                    id = self.next_id
-                    self.map.put(token, self.next_id)
-                    self.next_id += 1
-                
-                token_ids.append(id)
-
-        return (tokens, token_ids)
+            if id is None:
+                id = self.next_id
+                self.map.put(token, self.next_id)
+                self.next_id += 1
+            
+        return id
 
     def encode_pad(self) -> int:
         return self.map[PAD_TOKEN]
-    
+
     def decode(self, id: int) -> str:
         return self.map.inverse[id]
